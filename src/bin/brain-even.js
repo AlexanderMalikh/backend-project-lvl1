320060221481fdc51c2e3cb3bb1685a9f658d1ce
Alexander Malykh <fulgrimka1@gmail.com>
2019-09-08 23:54:42 +0300

    completed step 4

---
+++ src/bin/brain-even.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { guessEven, greetUser } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+greetUser();
+guessEven();
--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@
 
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
-  const userName = readlineSync.question('May i have your name?');
-  console.log(`Hello, ${userName}`);
+let userName = '';
+
+export const greetUser = () => {
+  userName = readlineSync.question('May i have your name?');
+  console.log(`Hello, ${userName}\n`);
 };
 
-export default greetUser;
+export const guessEven = () => {
+  let rightAnswerCounter = 0;
+  while (rightAnswerCounter < 3) {
+    const questionNumber = Math.floor(Math.random() * (101));
+    const userAnswer = readlineSync.question(`Question: ${questionNumber}`);
+    if (questionNumber % 2 === 0 && userAnswer === 'yes') {
+      rightAnswerCounter += 1;
+      console.log('Correct!');
+    } else if (questionNumber % 2 !== 0 && userAnswer === 'no') {
+      rightAnswerCounter += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`Let's try again, ${userName}`);
+      break;
+    }
+  }
+  if (rightAnswerCounter === 3) console.log(`Congratulations, ${userName}!`);
+};
